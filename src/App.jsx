af68f72f23d0a236ce32b3162d2d4128f506c012
Mehul <mehulr2801@gmail.com>
2025-03-30 02:21:26 +0530

    name fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import SignupForm from "./components/SignupForm";
-import TermsAndConditions from "./components/TermsAndConditions.jsx";
+import TermsAndConditions from "./components/TermsAndConditions";
 
 export default function App() {
   return (
