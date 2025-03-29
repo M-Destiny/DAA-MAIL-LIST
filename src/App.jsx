64cd494bcaa456f7a74f8abec0275985c2b5582b
Mehul <mehulr2801@gmail.com>
2025-03-30 02:19:59 +0530

    jsx?

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import SignupForm from "./components/SignupForm";
-import TermsAndConditions from "./components/TermsAndConditions";
+import TermsAndConditions from "./components/TermsAndConditions.jsx";
 
 export default function App() {
   return (
